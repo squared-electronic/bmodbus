b621daff3698f34da02c7ce5a9dcacddec7fc3fe
billmccartney <billmccartney@gmail.com>
2025-03-28 19:32:18 -0400

    Fixed type information.

--- tests/client/test_bmodbus_client.c
+++ tests/client/test_bmodbus_client.c
@@ -21,11 +21,10 @@ void tearDown(void) {
 
 void test_bmodbus_client_simple(void) {
     uint8_t writing_registers_address_0x0708_at_slave_2[] = {0x02, 0x10, 0x07, 0x08, 0x00, 0x02, 0x04, 0x02, 0x03, 0x04, 0x05, 0xe8, 0x06, };
-    int index;
     modbus1_init(2);
     uint32_t fake_time = 0;
     fake_time += byte_timing_microseconds(38400) * 1; // 1 byte
-    for(int i=0;i<sizeof(writing_registers_address_0x0708_at_slave_2);i++) {
+    for(uint16_t i=0;i<sizeof(writing_registers_address_0x0708_at_slave_2);i++) {
         modbus1_next_byte(fake_time, writing_registers_address_0x0708_at_slave_2[i]);
         fake_time += byte_timing_microseconds(38400) * 1; // 1 byte
     }
