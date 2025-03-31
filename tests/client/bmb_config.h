2102fd6c57f809b2bb5d83829864fb3c0c10c056
billmccartney <billmccartney@gmail.com>
2025-03-31 13:48:08 -0400

    Removed memmove dependency.

--- tests/client/bmb_config.h
+++ tests/client/bmb_config.h
@@ -5,6 +5,6 @@
 #ifndef BMODBUS_BMB_CONFIG_H
 #define BMODBUS_BMB_CONFIG_H
 #define BMB1_CLIENT
-#include <string.h>
-#define MODBUS_MEMMOVE memmove
+//#include <string.h>
+//#define MODBUS_MEMMOVE memmove
 #endif //BMODBUS_BMB_CONFIG_H
