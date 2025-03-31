92f0befa8c177113b1a74e0a114f48c70ed6849d
billmccartney <billmccartney@gmail.com>
2025-03-31 13:04:36 -0400

    Added unit tests for single/multiple register reads.

--- tests/client/test_bmodbus_client.c
+++ tests/client/test_bmodbus_client.c
@@ -68,3 +68,76 @@ void test_client_single_write(void){
     TEST_ASSERT_NOT_EQUAL_MESSAGE(NULL, response, "modbus response is not ready");
     TEST_ASSERT_EQUAL_MESSAGE(8, response->size, "response->size");
 }
+
+void test_client_single_read(void){
+    uint8_t reading_register_address_0x0708_at_slave_2[] = {0x02, 0x03, 0x07, 0x08, 0x00, 0x01, 0x04, 0x8f, };
+    modbus_request_t * request = NULL;
+    modbus_uart_response_t * response = NULL;
+    modbus1_init(2);
+    uint32_t fake_time = 0;
+    fake_time += byte_timing_microseconds(38400) * 1; // 1 byte
+    for(uint16_t i=0;i<sizeof(reading_register_address_0x0708_at_slave_2);i++) {
+        //Ensure we are not giving a response
+        TEST_ASSERT_EQUAL_MESSAGE(NULL, modbus1_get_request(), "modbus request is ready too early");
+        modbus1_next_byte(fake_time, reading_register_address_0x0708_at_slave_2[i]);
+        fake_time += byte_timing_microseconds(38400) * 1; // 1 byte
+    }
+    request = modbus1_get_request();
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(NULL, request, "modbus request is ready too early");
+    TEST_ASSERT_EQUAL(CLIENT_STATE_PROCESSING_REQUEST, modbus1_testing->state);
+    TEST_ASSERT_EQUAL(0x03, request->function);
+    TEST_ASSERT_EQUAL(0x0708, request->address);
+    TEST_ASSERT_EQUAL(0x01, request->size);
+
+    request->data[0] = 0xdead;
+    request->data[1] = 0xbeef;
+    request->data[2] = 0xd00d;
+
+    response = modbus1_get_response();
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(NULL, response, "modbus response is not ready");
+    TEST_ASSERT_EQUAL(5+2*3, response->size);
+    TEST_ASSERT_EQUAL(0x02, response->data[0]);
+    TEST_ASSERT_EQUAL(0x03, response->data[1]);
+    TEST_ASSERT_EQUAL(0x06, response->data[2]);
+    TEST_ASSERT_EQUAL(0xde, response->data[3]);
+    TEST_ASSERT_EQUAL(0xad, response->data[4]);
+    TEST_ASSERT_EQUAL(0xbe, response->data[5]);
+    TEST_ASSERT_EQUAL(0xef, response->data[6]);
+    TEST_ASSERT_EQUAL(0xd0, response->data[7]);
+    TEST_ASSERT_EQUAL(0x0d, response->data[8]);
+}
+
+void test_client_multiple_read(void){
+    uint8_t reading_registers_address_0x0708_at_slave_2[] = {0x02, 0x04, 0x07, 0x08, 0x00, 0x03, 0x30, 0x8e, };
+    modbus_request_t * request = NULL;
+    modbus_uart_response_t * response = NULL;
+    modbus1_init(2);
+    uint32_t fake_time = 0;
+    fake_time += byte_timing_microseconds(38400) * 1; // 1 byte
+    for(uint16_t i=0;i<sizeof(reading_registers_address_0x0708_at_slave_2);i++) {
+        //Ensure we are not giving a response
+        TEST_ASSERT_EQUAL_MESSAGE(NULL, modbus1_get_request(), "modbus request is ready too early");
+        modbus1_next_byte(fake_time, reading_registers_address_0x0708_at_slave_2[i]);
+        fake_time += byte_timing_microseconds(38400) * 1; // 1 byte
+    }
+    request = modbus1_get_request();
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(NULL, request, "modbus request is ready too early");
+    TEST_ASSERT_EQUAL(CLIENT_STATE_PROCESSING_REQUEST, modbus1_testing->state);
+    TEST_ASSERT_EQUAL(0x04, request->function);
+    TEST_ASSERT_EQUAL(0x0708, request->address);
+    TEST_ASSERT_EQUAL(0x01, request->size);
+
+    request->data[0] = 0xdead;
+    request->data[1] = 0xbeef;
+    request->data[2] = 0xd00d;
+
+    response = modbus1_get_response();
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(NULL, response, "modbus response is not ready");
+    TEST_ASSERT_EQUAL(5+2*3, response->size);
+    TEST_ASSERT_EQUAL(0x02, response->data[0]);
+    TEST_ASSERT_EQUAL(0x04, response->data[1]);
+    TEST_ASSERT_EQUAL(0x06, response->data[2]);
+    TEST_ASSERT_EQUAL(0xde, response->data[3]);
+    TEST_ASSERT_NOT_EQUAL(0xad, response->data[4]);
+    TEST_ASSERT_NOT_EQUAL(0xbe, response->data[5]);
+}
--- tests/test_runner.c
+++ tests/test_runner.c
@@ -5,12 +5,17 @@
 #include <stdint.h>
 extern void test_client_simple_write(void);
 extern void test_client_single_write(void);
+extern void test_client_single_read(void);
+extern void test_client_multiple_read(void);
+
 int main(void) {
     UNITY_BEGIN();
 
     // Run test cases
     RUN_TEST(test_client_simple_write);
     RUN_TEST(test_client_single_write);
+    RUN_TEST(test_client_single_read);
+    RUN_TEST(test_client_multiple_read);
 
     return UNITY_END();
 }
