cbc7448aa1a62511925c25e17af241c9d28e7842
billmccartney <billmccartney@gmail.com>
2025-06-03 19:23:16 -0400

    Removed some dead code in the unit tests.

--- tests/arduino/arduino_unit_tests/arduino_unit_tests.ino
+++ tests/arduino/arduino_unit_tests/arduino_unit_tests.ino
@@ -1,7 +1,4 @@
 #define FAKE_MAIN
-#define UNITY_OMIT_OUTPUT_CHAR_HEADER_DECLARATION
-//#define UNITY_OUTPUT_CHAR(x) Serial.write((uint8_t)x)
-//#include "../../client/test_modbus_client.c"
 #include "test_bmodbus_client.h"
 
 extern "C" void uut_serial_write(const int c){
