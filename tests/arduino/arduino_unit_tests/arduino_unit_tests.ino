03de40f1c8da8613931ca87e770d8da3dba43d5a
billmccartney <billmccartney@gmail.com>
2025-06-03 18:21:09 -0400

    Added more printouts for the unit tests on arduino.

--- tests/arduino/arduino_unit_tests/arduino_unit_tests.ino
+++ tests/arduino/arduino_unit_tests/arduino_unit_tests.ino
@@ -4,16 +4,19 @@
 //#include "../../client/test_modbus_client.c"
 #include "test_bmodbus_client.h"
 
-extern "C" void uut_serial_write(int c){
+extern "C" void uut_serial_write(const int c){
     Serial.write((uint8_t)c);
+    Serial.flush();
 }
 
 void setup(){
     Serial.begin(115200);
+    Serial.println("Booted");
 }
 
 void loop(){
     delay(100);
+    Serial.println("Before tests");
     unit_test_main();
     Serial.println("All tests completed");
 }
